1330461a97c16eaafa2aed283a1cde1ceba39d51
Vadim TS <vadimhm@yandex.ru>
2021-05-08 14:24:37 +0300

    Fix. причесал код

--- web/index.php
+++ web/index.php
@@ -1,16 +1,15 @@
 <?php
 
-require dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $uuid = vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex(random_bytes(16)), 4));
 $userUUID = null;
+$total = 0;
 
-
-if (isset($_COOKIE['_welcome-client'])){
+if (isset($_COOKIE['_welcome-client']) && !empty($_COOKIE['_welcome-client'])) {
     $userUUID = $_COOKIE['_welcome-client'];
-}else{
-   $SS=  setcookie("_welcome-client", $uuid, strtotime('+30 sec'));
+} else {
+    setcookie("_welcome-client", $uuid, strtotime('+1day'));
     $userUUID = $uuid;
 }
 
@@ -22,57 +21,33 @@ $redis->auth($redisAuth);
 
 
 if ($redis->ping()) {
-//    echo "PONGn";
-
-
+    /** Insert user id with life time */
+    $redis->setEx($userUUID, 30, strtotime('+30 sec'));
+    $total = $redis->dbSize();
+} else {
+    throw new RedisException("Ошибка соединения с Redis сервером");
 }
-/** Записываем нового пользователя, если его еще нету.   */
-//if($redis->setNx($userUUID, strtotime('+30 sec'))){}
-/** Insert user id with life time */
-$redis->setEx($userUUID,30, strtotime('+30 sec'));
-//$redis->pSetEx($userUUID,30, strtotime('+30 sec'));
-$total = $redis->dbSize();
-//var_dump($total);
-//var_dump($redis->getDbNum());
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        .center {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 120px;
-        }
-        .loader {
-            border: 16px solid #f3f3f3;
-            border-radius: 50%;
-            border-top: 16px solid #198754 ;
-            border-bottom: 16px solid #198754 ;
-            width: 120px;
-            height: 120px;
-        }
-        .label-counter{
-            color: #198754;
-            text-align: center;
-            font-size: 40pt;
-        }
-    </style>
+    <title>Количество пользователей на сайте</title>
+    <link href='/css/style.css' rel='stylesheet'>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <body>
 <h1 class="center">Сейчас на сайте!</h1>
-
 <div class="center">
     <div class="loader">
         <div class="center">
-            <p class="label-counter"><?=$total?></p>
+            <p class="label-counter"><?= $total ?></p>
         </div>
     </div>
 </div>
-
 </body>
 </html>
 
